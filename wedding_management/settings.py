5c7e8b063f3cec558b5770558195bd1333ef17b2
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-07-02 19:59:39 -0300

    Trocado o nome do app contrato para contracts

--- wedding_management/settings.py
+++ wedding_management/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # apps
     'apps.calendar',
-    'apps.contratos',
+    'apps.contracts',
     'apps.itens',
     'apps.orcamento',
     'apps.core',
