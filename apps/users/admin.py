0dbbccc5f1bc83b6eae3609c00c60282fed21217
FlavioProgramador <flavionegocios2004@gmail.com>
2025-07-10 21:12:26 -0300

    feat: register user models and configure admin interface

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Cerimonialista, Cliente, Fornecedor
+
+@admin.register(Cerimonialista)
+class CerimonialistaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf_cnpj', 'telefone')
+    search_fields = ('nome', 'email', 'cpf_cnpj')
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf', 'telefone')
+    search_fields = ('nome', 'email', 'cpf')
+
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf_cnpj', 'telefone', 'servicos_oferecidos')
+    search_fields = ('nome', 'email', 'cpf_cnpj')
--- wedding_management/settings.py
+++ wedding_management/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "apps.items",
     "apps.budget",
     "apps.core",
+    "apps.users",
 ]
 
 MIDDLEWARE = [
