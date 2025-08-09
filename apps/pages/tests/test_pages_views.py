4e5113d8f1a72f40dd28232271a9def0272204b8
Rafael Pereira <rafaelpereiradearaujo5@gmail.com>
2025-08-09 14:30:58 -0300

    test: add unit tests for contact us and home pages to verify view, status code, template, and content

--- apps/pages/tests/test_pages_views.py
+++ apps/pages/tests/test_pages_views.py
@@ -10,3 +10,33 @@ class TestPagesViews(TestCase):
             reverse("pages:home")
         )
         self.assertIs(view.func, views.home)
+
+    def test_pages_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse("pages:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_pages_home_template_is_correct(self):
+        response = self.client.get(reverse("pages:home"))
+        self.assertTemplateUsed(response, "pages/home.html")
+
+    def test_pages_home_contains_expected_content(self):
+        response = self.client.get(reverse("pages:home"))
+        self.assertContains(response, "Gestão de Casamentos")
+
+    def test_pages_contact_us_view_is_correct(self):
+        view = resolve(
+            reverse("pages:contact_us")
+        )
+        self.assertIs(view.func, views.contact_us)
+
+    def test_pages_contact_us_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse("pages:contact_us"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_pages_contact_us_template_is_correct(self):
+        response = self.client.get(reverse("pages:contact_us"))
+        self.assertTemplateUsed(response, "pages/contact-us.html")
+
+    def test_pages_contact_us_contains_expected_content(self):
+        response = self.client.get(reverse("pages:contact_us"))
+        self.assertContains(response, "Fale Conosco")
