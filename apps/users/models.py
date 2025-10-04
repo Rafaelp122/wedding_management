ca8731aa7d6a8be6da8e3ebb14d5b08cbb50efa1
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-10-04 04:14:43 -0300

    feat: implement custom user model with user manager for enhanced user management

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,13 +1,45 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.validators import MaxLengthValidator
 from django.db import models
 
 
-class Planner(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    cpf_cnpj = models.CharField(max_length=20, unique=True)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O campo de E-mail é obrigatório')
+        if not username:
+            raise ValueError('O campo de Username é obrigatório')
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, password, **extra_fields)
+
+
+class User(AbstractUser):
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
+    username = models.CharField(
+        max_length=255,
+        unique=True,
+        validators=[MaxLengthValidator(255)]
+    )
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.name
+        return self.email
