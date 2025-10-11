095c47a8d6475bcdd489bd64f53f734d11d99f25
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-10-11 15:22:09 -0300

    feat(mixins): add FormStylingMixin for consistent form styling across the application

---
+++ apps/core/utils/mixins.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .django_forms import add_attr
+
+
+class FormStylingMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.CheckboxInput):
+                add_attr(field, 'class', 'form-check-input')
+            else:
+                add_attr(field, 'class', 'form-control form-control-lg ps-5 custom-font-size')
+
+            if field_name in self.errors:
+                add_attr(field, 'class', 'is-invalid')
