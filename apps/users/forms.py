ef65d19a61735099c79b174d72ea26e308ec9592
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-10-12 23:14:07 -0300

    fix(users): Fix validation in the user creation form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -12,47 +12,37 @@ from .models import User
 
 
 class CustomUserCreationForm(FormStylingMixinLarge, UserCreationForm):
-    email = forms.EmailField(label="E-mail")
+    # Apenas campos NOVOS são declarados aqui
+    email = forms.EmailField(
+        label="E-mail",
+        help_text="O e-mail é obrigatório."
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        # Adicione os campos que você quer do UserCreationForm + o seu novo campo 'email'
+        fields = ("username", "email", "first_name", "last_name")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.fields['username'].label = "Usuário"
+        self.fields['first_name'].label = "Primeiro Nome"
+        self.fields['last_name'].label = "Último Nome"
+
+        self.fields['password1'].label = "Senha"
+        self.fields['password1'].help_text = (
+            "Senha com pelo menos 8 caracteres, não totalmente "
+            "numérica e pouco comum."
+        )
+        self.fields['password2'].label = "Confirme a senha"
+
         add_placeholder(self.fields['username'], 'Digite seu usuário')
         add_placeholder(self.fields['email'], 'seu@email.com')
         add_placeholder(self.fields['first_name'], 'Seu primeiro nome')
-        add_placeholder(self.fields['last_name'], 'Seu ultimo nome')
+        add_placeholder(self.fields['last_name'], 'Seu último nome')
         add_placeholder(self.fields['password1'], 'Sua senha')
         add_placeholder(self.fields['password2'], 'Repita sua senha')
-
-    class Meta:
-        model = User
-        fields = [
-            "username",
-            "email",
-            "first_name",
-            "last_name",
-            "password1",
-            "password2",
-            ]
-
-    username = forms.CharField(
-        label="Usuário",
-    )
-
-    password1 = forms.CharField(
-        label="Senha",
-        widget=forms.PasswordInput(),
-        help_text=(
-            "Senha com pelo menos 8 caracteres, não totalmente "
-            "numérica e pouco comum."
-        ),
-    )
-
-    # password2 = forms.CharField(
-    #     label="Confirme a senha",
-    #     widget=forms.PasswordInput(),
-    #     help_text='',
-    # )
 
 
 class SignInForm(FormStylingMixinLarge, AuthenticationForm):
