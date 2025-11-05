187a6556a0f4928c357c29b7ad4f2e857078ea67
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-11-05 11:27:30 -0300

    refactor(items): Replace function-based views with class-based views for item management

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from . import views
+
+from .views import AddItemView, PartialItemsView
 
 app_name = "items"
 
 urlpatterns = [
-    path("partial/<int:wedding_id>/", views.partial_items, name="partial_items"),
-    path("add/<int:wedding_id>/", views.add_item, name="add_item"),
-    # Adicione uma URL para listar os itens, que será usada para cancelar
-    # path('list/<int:wedding_id>/', views.list_items, name='list_items'),
+    path(
+        '<int:wedding_id>/items/partial/',
+        PartialItemsView.as_view(),
+        name='partial_items'
+    ),
+    path(
+        '<int:wedding_id>/items/add/',
+        AddItemView.as_view(),
+        name='add_item'),
 ]
