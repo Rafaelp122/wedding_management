cf59e7933f41a40cfb53b3a3e5e9de8e0c12e0df
FlavioProgramador <flavionegocios2004@gmail.com>
2025-11-10 12:48:44 -0300

    Comentarios dos models do app 'Client'.

--- apps/client/models.py
+++ apps/client/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 
 class Client(models.Model):
+    # Modelo que representa um cliente do sistema
     name = models.CharField(max_length=255)
     cpf = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
 
+   # Exibe o nome do cliente como representação do objeto
     def __str__(self):
         return self.name
