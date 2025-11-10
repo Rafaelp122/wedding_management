b2937c9e917d9949ca2f0e3c2c245d7de1c76cdd
FlavioProgramador <flavionegocios2004@gmail.com>
2025-11-10 12:53:07 -0300

    Comntários no admin do app 'Contracts'.

--- apps/contracts/admin.py
+++ apps/contracts/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Contract
 
 
+# Configuração do modelo Contract no painel administrativo do Django
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
-    list_display = ("id", "wedding", "created_at", "status")
-    search_fields = ("wedding__client__name",)
-    list_filter = ("status", "wedding")
+    list_display = ("id", "wedding", "created_at", "status")  # Campos mostrados na listagem
+    search_fields = ("wedding__client__name",)  # Permite buscar pelo nome do cliente do casamento
+    list_filter = ("status", "wedding")  # Filtros laterais para facilitar a navegação
