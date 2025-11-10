a68c98c1b2bac00712a9877637fd4098edbe6e60
FlavioProgramador <flavionegocios2004@gmail.com>
2025-11-10 13:16:12 -0300

    docs(apps/pages): adiciona comentários simples nas rotas das páginas estáticas

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from . import views
 
+# Define o namespace para o app de pages
 app_name = "pages"
 
+# Rotas das páginas estáticas do site
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
-    path("fale-conosco/", views.ContactUsView.as_view(), name="contact_us"),
+    path("", views.HomeView.as_view(), name="home"),  # Página inicial
+    path("fale-conosco/", views.ContactUsView.as_view(), name="contact_us"),  # Página de contato
 ]
