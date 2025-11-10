180e125e483caec3c0920e4516db5103388eb3be
FlavioProgramador <flavionegocios2004@gmail.com>
2025-11-10 12:55:31 -0300

    Comentários nas urls do app 'Contracts'.

--- apps/contracts/urls.py
+++ apps/contracts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-
 from .views import ContractsPartialView
 
+
+# Define o namespace para o app de contratos
 app_name = "contracts"
 
+# Rotas do app de contratos
 urlpatterns = [
+    # Exibe a lista parcial de contratos de um casamento específico
     path(
         "partial/<int:wedding_id>/",
         ContractsPartialView.as_view(),
