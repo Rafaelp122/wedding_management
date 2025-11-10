a72493760b989c1a13132bbc1c97a809b99be70e
FlavioProgramador <flavionegocios2004@gmail.com>
2025-11-10 12:50:19 -0300

    Comentarios no admin do app 'Client'.

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-
 from .models import Client
 
 
+# Configuração de exibição do modelo Client no painel admin
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "cpf", "phone")
-    search_fields = ("name", "email", "cpf")
+    list_display = ("name", "email", "cpf", "phone")  # Campos mostrados na lista
+    search_fields = ("name", "email", "cpf")  # Campos pesquisáveis no admin
