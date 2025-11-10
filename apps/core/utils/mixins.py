47453f7e8fbbd8344c03d72b7bcb5e1463a83a1d
FlavioProgramador <flavionegocios2004@gmail.com>
2025-11-10 13:01:35 -0300

    docs(core/utils): adiciona comentários nos mixins de estilização de formulários

--- apps/core/utils/mixins.py
+++ apps/core/utils/mixins.py
@@ -1,12 +1,13 @@
 from django import forms
-
 from .django_forms import add_attr
 
 
+# Mixin que aplica classes CSS padrão aos campos de formulário
 class FormStylingMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Adiciona classes de estilo para cada campo do formulário
         for field_name, field in self.fields.items():
             if isinstance(field.widget, forms.CheckboxInput):
                 add_attr(field, "class", "form-check-input")
@@ -16,6 +17,7 @@ class FormStylingMixin:
     def _post_clean(self):
         super()._post_clean()
 
+        # Marca os campos com erro como inválidos (para exibir feedback visual)
         if self.errors:
             for field_name in self.errors:
                 field = self.fields.get(field_name)
@@ -23,10 +25,12 @@ class FormStylingMixin:
                     add_attr(field, "class", "is-invalid")
 
 
+# Versão do mixin com campos maiores (usado em formulários mais destacados)
 class FormStylingMixinLarge:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Aplica classes de estilo em tamanho grande
         for field_name, field in self.fields.items():
             if isinstance(field.widget, forms.CheckboxInput):
                 add_attr(field, "class", "form-check-input")
@@ -38,6 +42,7 @@ class FormStylingMixinLarge:
     def _post_clean(self):
         super()._post_clean()
 
+        # Marca os campos inválidos com a classe CSS de erro
         if self.errors:
             for field_name in self.errors:
                 field = self.fields.get(field_name)
