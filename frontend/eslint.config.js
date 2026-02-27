150353544b5fc7845b0a866475ca48266191aab7
Rafael <rafaelpereiradearaujo5@gmail.com>
2026-02-27 19:13:56 -0300

    refactor(frontend): configure path aliases and reorganize project structure

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,12 +6,15 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  // Arquivos e pastas ignorados globalmente
   globalIgnores(["dist", "node_modules", "src/api/generated/**/*"]),
+
+  // 2. Configuração Global
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended, // uso de spread para garantir compatibilidade
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -22,10 +25,22 @@ export default defineConfig([
     rules: {
       "@typescript-eslint/no-empty-object-type": [
         "error",
-        {
-          allowInterfaces: "with-single-extends",
-        },
+        { allowInterfaces: "with-single-extends" },
       ],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+
+  // Exceção específica para o shadcn/ui
+  {
+    // Aplica-se apenas aos arquivos dentro da pasta ui
+    files: ["src/components/ui/**/*.{ts,tsx}"],
+    rules: {
+      // Desativa o aviso que o shadcn causa propositalmente
+      "react-refresh/only-export-components": "off",
     },
   },
 ]);
